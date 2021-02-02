ec14ca1eddd1c3fd7562db79c5f37179e1c3729a
Kel Strother <kelstrother@gmail.com>
2021-02-02 13:18:34 -0700

    deployment2 commit

--- db/connection.js
+++ db/connection.js
@@ -1,2 +1,13 @@
+
+require("dotenv").config()
+const { MONGODBURI } = process.env;
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/' + 'guitars_db'
+const config = { useUnifiedTopology: true, useNewUrlParser: true }
+const db = mongoose.connection
+mongoose.connect(MONGODBURI, config)
+
+DB.on("open", () => console.log("You are connected to Mongo"))
+  .on("close", () => console.log("You are disconnected to Mongo"))
+  .on("error", (err) => console.log(err));
+
+module.exports = mongoose;
